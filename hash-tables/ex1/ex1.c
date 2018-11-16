c8c4fe3cd85bcceecb231bb0818e5dd9826ec1a4
Charlie Winslow <cwinslow22@yahoo.com>
2018-11-16 12:58:54 -0600

    more progress on ex1, it works for make, but doesnt pass make tests yet

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -15,7 +15,7 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 
   for (int i = 0; i < length; i++) {
     if (hash_table_retrieve(ht, limit - weights[i]) != -1) {
-      if (weights[i] > (limit - weights[i])) {
+      if (weights[i] < (limit - weights[i])) {
         answer->index_1 = hash_table_retrieve(ht, weights[i]);
         answer->index_2 = hash_table_retrieve(ht, limit - weights[i]);
       } else {
@@ -25,7 +25,7 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
       return answer;
     }
   }
-
+  printf("%d %d\n", answer->index_1, answer->index_2);
   return NULL;
 }
 
@@ -37,3 +37,20 @@ void print_answer(Answer *answer)
     printf("NULL\n");
   }
 }
+
+#ifndef TESTING
+int main(void)
+{
+  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+
+  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  printf("%d %d\n", answer_4->index_1, answer_4->index_2);
+
+  if (answer_4->index_1 != 6)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}\n");
+  if (answer_4->index_2 != 2)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}\n");
+
+  return 0;
+}
+#endif
