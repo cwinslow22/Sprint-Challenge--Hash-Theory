2124b410f88f03cef7e4076b21a2f59572727a69
Charlie Winslow <cwinslow22@yahoo.com>
2018-11-16 12:40:19 -0600

    progress check on ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,13 +6,20 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *answer = malloc(sizeof(Answer));
 
   // YOUR CODE HERE
   for (int i = 0; i < length; i++) {
     hash_table_insert(ht, weights[i], i)
   }
 
-  
+  for (int i = 0; i < length; i++) {
+    if (hash_table_retrieve(limit - weights[i]) != -1) {
+      if (weights[i] > (limit - weights[i])) {
+        
+      }
+    }
+  }
 
   return NULL;
 }
